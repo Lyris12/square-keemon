be8089c254d49db1a182e13fcbceb9ff25e9681b
Lyris12 <lyris89@live.com>
2023-11-29 18:27:48 -0500

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,5 @@ import ReactDOM from 'react-dom/client'
 import Square from './lib/index'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Square size={250} direction="DTL" smooth />,
+  <Square size={250} direction="DTL" />,
 )
